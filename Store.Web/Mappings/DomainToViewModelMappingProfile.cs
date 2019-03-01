ae98072cbbadfe6e6145437dfadb14c9bba6744e
Mawuli Mensah <mawuliusk@gmail.com>
2019-03-01 13:03:39 +0000

    first commit

---
+++ Store.Web/Mappings/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Store.Web.ViewModels;
+using Store.Model;
+
+namespace Store.Web.Mappings
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Category, CategoryViewModel>();
+            Mapper.CreateMap<Gadget, GadgetViewModel>();
+        }
+    }
+}
